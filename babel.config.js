5eeb9d4cd8f8cd1546ebf5b73e9e9df10b40d406
Saad <139798250+Warrior005@users.noreply.github.com>
2026-03-07 00:31:54 +0530

    fix: update babel configuration to include nativewind preset and correct plugin usage

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,12 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }]],
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel"
+    ],
     plugins: [
-      "nativewind/babel",
-      "react-native-worklets/plugin"
+      "react-native-reanimated/plugin"
     ],
   };
 };
